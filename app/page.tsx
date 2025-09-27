e63bc12d79ede806e9f8e7007c5e128c4a14438f
Akash <airshula79@gmail.com>
2025-09-27 22:27:52 +0700

    installed zod and shadcn , completed form for username only.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,68 @@
+"use client"
 import Image from "next/image";
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { UserSchema } from "@/schema/Validations";
+
+
+
+
+
 export default function Home() {
+  const onSubmit = async (data: z.infer<typeof UserSchema>) => {
+  console.log(data)
+}
+
+
+  const form = useForm<z.infer<typeof UserSchema>>({
+    resolver: zodResolver(UserSchema),
+    defaultValues: {
+      id: null,
+      username: "",
+      email: "",
+      mobile_number: "",
+      role: [{ role: "" }],
+    },
+});
   return (
     
     <div>
       {/* Create a form to create a new user using zod */}
-      
+      <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Joe" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
     </div>
   );
 }
---
+++ schema/Validations.ts
@@ -0,0 +1,10 @@
+import * as z from "zod";
+
+export const UserSchema = z.object({
+    id: z.number().nullable().optional(),
+    username: z.string().nonempty('Username is required'),
+    email: z.string().nonempty('Email is required'),
+    mobile_number: z.string().nonempty('Mobile Number is required'),
+    role: z.array(z.object({ role: z.string() })).nonempty('Role is required'),
+});
+
